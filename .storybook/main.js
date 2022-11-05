3d1d06443f896e6266514d1bdc7dacc77d78bbff
jinyoung <sm099@naver.com>
2022-11-05 18:50:37 +0900

    chore : storybook settings

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
+    "storybook-addon-next",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
