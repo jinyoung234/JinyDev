7f5e138963653ba7b8996a6093f108645d3a3406
jinyoung <sm099@naver.com>
2022-11-07 02:03:36 +0900

    Remove : Layout remove for folder constructor modify

--- components/Layout.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/jsx-no-useless-fragment */
-import React from "react";
-
-const Layout: React.FC<any> = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default Layout;
