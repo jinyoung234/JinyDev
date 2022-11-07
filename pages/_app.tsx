2243f82e71e24a024face54e91c8de7b5df4b36e
jinyoung <sm099@naver.com>
2022-11-07 20:13:03 +0900

    chore : settings recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
-/* eslint-disable import/order */
-/* eslint-disable @typescript-eslint/ban-types */
-import { NextPage } from "next";
 import { AppProps } from "next/app";
 import Layout from "../components/layout";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "../styles/globalStyle";
-import { colorTheme, lightTheme } from "../styles/theme";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
+    <RecoilRoot>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ThemeProvider>
+    </RecoilRoot>
   );
 }
