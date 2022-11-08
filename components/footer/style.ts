7e83d72d53fc674e3b65bb49c0e8ed753da1002d
jinyoung <sm099@naver.com>
2022-11-08 16:30:51 +0900

    style : modify FooterWrapper FooterListWrapper

--- components/footer/style.ts
+++ components/footer/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { displayColumn } from "../../styles/cssStyle";
 
-export const FooterComponent = styled.footer`
+export const FooterWrapper = styled.footer`
   ${displayColumn}
   align-items: center;
   justify-content: center;
@@ -10,31 +10,32 @@ export const FooterComponent = styled.footer`
     font-size: 1.4rem;
     margin-bottom: 1.5rem;
   }
-  ul {
-    display: flex;
-    a {
-      margin: 0 0.8rem;
+`;
+
+export const FooterListWrapper = styled.ul`
+  display: flex;
+  a {
+    margin: 0 0.8rem;
+  }
+  a:first-child {
+    padding-bottom: 0.15rem;
+    margin: 0 0.6rem;
+    svg {
+      width: 3rem;
+      height: 3rem;
     }
-    a:first-child {
-      padding-bottom: 0.15rem;
-      margin: 0 0.6rem;
-      svg {
-        width: 3rem;
-        height: 3rem;
-      }
+  }
+  a:nth-child(2) {
+    svg {
+      width: 2.8rem;
+      height: 2.8rem;
     }
-    a:nth-child(2) {
-      svg {
-        width: 2.8rem;
-        height: 2.8rem;
-      }
-    }
-    a:last-child {
-      padding-top: 0.2rem;
-      svg {
-        width: 2.4rem;
-        height: 2.4rem;
-      }
+  }
+  a:last-child {
+    padding-top: 0.2rem;
+    svg {
+      width: 2.4rem;
+      height: 2.4rem;
     }
   }
 `;
