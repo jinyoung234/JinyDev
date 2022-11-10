2db99e3bd0849c0187928b12c2740a023b5fb985
jinyoung <sm099@naver.com>
2022-11-11 00:36:29 +0900

    feat : add atoms form filter tag

--- atoms/index.ts
+++ atoms/index.ts
@@ -4,3 +4,18 @@ export const changeMode = atom({
   key: "changeMode",
   default: false,
 });
+
+export const initializeAtom = atom({
+  key: "initializeAtom",
+  default: true,
+});
+
+export const isCategoryAtom = atom({
+  key: "isCategoryAtom",
+  default: false,
+});
+
+export const tagNameAtom = atom({
+  key: "isTagNameAtom",
+  default: "",
+});
