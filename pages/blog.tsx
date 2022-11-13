f71696ff4e3d3badfe6748ab5d4ed625399d1bae
jinyoung <sm099@naver.com>
2022-11-13 12:18:17 +0900

    Refactor : separate Tag Component for add Slug page

---
+++ pages/blog.tsx
@@ -0,0 +1,22 @@
+import BlogList from "../components/blogList";
+import Seo from "../components/seo";
+import { PostsMapProps } from "../interfaces/common";
+import getPost from "../utils/getPost";
+
+export default function Blog({ posts }: PostsMapProps) {
+  return (
+    <>
+      <Seo title="Blog" />
+      <BlogList posts={posts} />;
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const { posts } = getPost("dev");
+  return {
+    props: {
+      posts,
+    },
+  };
+}
