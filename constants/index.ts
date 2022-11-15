30701f30475d09daf48812571e8af9f808afa398
jinyoung <sm099@naver.com>
2022-11-15 22:39:12 +0900

    fix : divide categoryList(dev & blog page)

--- constants/index.ts
+++ constants/index.ts
@@ -4,4 +4,5 @@ export const CONTENT_TITLE = {
   accentContent: "'도전하는 개발자' ",
   name: "손진영 입니다 :)",
 };
-export const CATEGORY_LIST = ["All", "TypeScript", "JavaScript", "React", "Algorithm", "DevOps", "FrontEnd", "CS"];
+export const DEV_CATEGORY_LIST = ["All", "TypeScript", "JavaScript", "React", "Algorithm", "DevOps", "FrontEnd", "CS"];
+export const BLOG_CATEGORY_LIST = ["All", "Remind", "TIL", "Promise", "MonthlyDiary"];
