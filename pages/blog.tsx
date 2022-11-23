b2c03a19703e67ac2154573f03f5037f5d0a75c3
jinyoung <sm099@naver.com>
2022-11-24 01:22:38 +0900

    fix : add prop in all pages for seo settings

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,12 @@
 // import BlogList from "../components/blogList";
-import Seo from "../components/seo";
+// import Seo from "../components/seo";
 // import { PostsMapProps } from "../interfaces/common";
 // import getPost from "../utils/getPost";
 
 export default function Blog() {
   return (
     <>
-      <Seo title="Blog" />
+      {/* <Seo title="Blog" /> */}
       {/* <BlogList posts={posts} />; */}
       <div>개발 준비 중..</div>
     </>
