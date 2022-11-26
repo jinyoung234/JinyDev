8b1f55b628f9715cc15cc7bed6a83b84d78be6e7
jinyoung <sm099@naver.com>
2022-11-27 01:26:45 +0900

    style : modify style constructor in Category Tag component

--- components/dev-blog/categoryTag/style.ts
+++ components/dev-blog/categoryTag/style.ts
@@ -1,23 +1,20 @@
 import styled from "styled-components";
 
 export const CategoryTagWrapper = styled.div<{ isChangeMode: boolean }>`
-  text-align: center;
-  word-break: keep-all;
   width: 8rem;
-  margin-right: 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   background-color: ${props => props.theme.accentColor.contentColor};
-  color: ${props => props.isChangeMode && props.theme.fontColor};
-  font-size: 1.2rem;
-  padding: 0.4rem 0;
   border-radius: 1rem;
-  line-height: 1.3;
   box-shadow: ${props => props.theme.shadow.defaultShadow};
   :hover {
     background-color: ${props => (props.isChangeMode ? "#8AB633" : "#dee2e6")};
     transition: background-color 0.2s ease-in-out;
     cursor: pointer;
+  }
+  span {
+    line-height: 1.3;
+    font-size: 1.2rem;
+    text-align: center;
+    word-break: keep-all;
+    color: ${props => props.isChangeMode && props.theme.fontColor};
   }
 `;
