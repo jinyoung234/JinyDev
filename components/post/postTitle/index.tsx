d14a5f90a2bdcb6794bcca4f7fb9b1f54010d198
jinyoung <sm099@naver.com>
2022-11-27 12:41:01 +0900

    style : modify style constructor in PostTitle component

--- components/post/postTitle/index.tsx
+++ components/post/postTitle/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FrontMatterProps } from "../../../interfaces/common";
 import Tag from "../../common/tag";
+import { PostTitleWrapper } from "./style";
 
 export interface PostTitleProps {
   frontMatter: FrontMatterProps;
@@ -8,12 +9,12 @@ export interface PostTitleProps {
 
 export default function PostTitle({ frontMatter }: PostTitleProps) {
   return (
-    <div id="frontmatter">
+    <PostTitleWrapper>
       <p id="category">{frontMatter.category}</p>
       <p id="title">{frontMatter.title}</p>
       <p id="createdAt">{frontMatter.createdAt.split(" ")[0]}</p>
       <Tag post={frontMatter} />
       <img src={frontMatter.thumbnailUrl} alt="jinyoung" />
-    </div>
+    </PostTitleWrapper>
   );
 }
