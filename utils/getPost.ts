046f4e2b019ea6a8fbc040572f7a6a325618cb99
jinyoung <sm099@naver.com>
2023-06-12 22:12:13 +0900

    fix : frontMatter에 첫 번째 객체가 빈 객체로 나오는 버그 수정

--- utils/getPost.ts
+++ utils/getPost.ts
@@ -3,7 +3,7 @@ import path from "path";
 import matter from "gray-matter";
 
 export default function getPost(category: string) {
-  const files = fs.readdirSync(path.join(`./posts/${category}`));
+  const files = fs.readdirSync(path.join(`./posts/${category}`)).slice(1);
   const posts = files.map(filename => {
     const markdownWithMeta = fs.readFileSync(path.join(`./posts/${category}`, filename), "utf-8");
     const { data: frontMatter } = matter(markdownWithMeta);
