841869d657aebe638352b5217be3a84ef5fb2ae7
jinyoung <sm099@naver.com>
2023-06-12 22:12:41 +0900

    feat : blog 페이지 추가

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,23 +1,22 @@
-// import BlogList from "../components/blogList";
-// import Seo from "../components/seo";
-// import { PostsMapProps } from "../interfaces/common";
-// import getPost from "../utils/getPost";
+import Seo from "../components/common/seo";
+import { BlogProps } from "../interfaces/common";
+import Blog from "../templates/blog";
+import getPost from "../utils/getPost";
 
-export default function BlogPage() {
+export default function BlogPage({ posts }: BlogProps) {
   return (
     <>
-      {/* <Seo title="Blog" /> */}
-      {/* <BlogList posts={posts} />; */}
-      <div>개발 준비 중..</div>
+      <Seo keywords={["개발자", "프론트엔드", "손진영"]} description="개발자로써의 이야기를 적는 공간" title="Blog" />
+      <Blog posts={posts} />
     </>
   );
 }
 
-// export async function getStaticProps() {
-//   const { posts } = getPost("blog");
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  const { posts } = getPost("blog");
+  return {
+    props: {
+      posts,
+    },
+  };
+}
